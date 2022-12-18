205a43ad28d9d98afc14c0d5cddea5305489fff9
DimaDostoevskiy <dostoevskiy@list.ru>
2022-12-18 12:26:54 +0300

    create DeleteItemModal

---
+++ src/components/DeleteItemModal.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="delete-modal delete-modal_active seondary-BG primary-BORDER br-12"
+  >
+    <input
+      class="delete-modal__input seondary-BG primary-BORDER br-4"
+      type="text"
+      placeholder=" input quantyty"
+    />
+    <div class="delete-modal__buttons-wraper">
+      <button
+        class="delete-modal__button-cancel primary-BORDER br-8"
+        type="button"
+      >
+        cancel
+      </button>
+      <button
+        class="delete-modal__button-confirm primary-BORDER br-8"
+        type="button"
+      >
+        confirm
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.delete-modal {
+  position: absolute;
+
+  padding: 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-direction: column;
+
+  right: 0px;
+  bottom: -120px;
+
+  width: 250px;
+  height: 135px;
+}
+.delete-modal_active {
+  right: 33px;
+  bottom: 128px;
+}
+.delete-modal__input {
+  width: 210px;
+  height: 40px;
+  margin: 0 auto 20px auto;
+}
+.delete-modal__buttons-wraper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+}
+.delete-modal__button-cancel {
+  width: 88px;
+  height: 33px;
+  color: #2D2D2D;
+}
+.delete-modal__button-confirm {
+  width: 112px;
+  height: 33px;
+  background: #fa7272;
+  color: #ffff;
+}
+</style>
---
+++ src/components/InventoryModal.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="inventory-modal inventory-modal_active seondary-BG br-12">
+    <span class="inventory-modal__button_close">&times;</span>
+    <div class="inventory-modal__image"></div>
+    <div class="inventory-modal__line skeleton-BG"></div>
+    <div class="inventory-modal__stub1 skeleton-BG br-8"></div>
+    <div class="inventory-modal__stub2 skeleton-BG br-4"></div>
+    <div class="inventory-modal__stub2 skeleton-BG br-4"></div>
+    <div class="inventory-modal__stub2 skeleton-BG br-4"></div>
+    <div class="inventory-modal__stub3 skeleton-BG br-4"></div>
+    <div class="inventory-modal__stub4 skeleton-BG br-4"></div>
+    <div class="inventory-modal__line skeleton-BG"></div>
+    <div class="delete_button">
+      <div class="delete_button__text">Delete</div>
+    </div>
+  </div>
+  <DeleteItemModalVue />
+</template>
+
+<script setup>
+import DeleteItemModalVue from "./DeleteItemModal.vue";
+</script>
+
+<style scoped>
+.inventory-modal {
+  position: absolute;
+
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  flex-direction: column;
+
+  top: 32px;
+  right: -220px;
+
+  width: 250px;
+  height: 500px;
+
+  border: 1px solid #4d4d4d;
+}
+.inventory-modal_active {
+  right: 33px;
+}
+.inventory-modal__button_close {
+  position: absolute;
+  right: 10px;
+  top: -5px;
+  font-size: 30px;
+  font-weight: 600;
+}
+.inventory-modal__image {
+  margin: 55px auto 30px auto;
+  width: 130px;
+  height: 130px;
+
+  background-image: url("../assets/images/Item_Image_green.png");
+}
+.inventory-modal__line {
+  margin: 0 auto 15px auto;
+
+  width: 90%;
+  height: 2px;
+}
+.inventory-modal__stub1 {
+  margin: 0 auto 25px auto;
+  width: 210px;
+  height: 30px;
+}
+.inventory-modal__stub2 {
+  margin: 0 auto 16px auto;
+  width: 210px;
+  height: 10px;
+}
+.inventory-modal__stub3 {
+  margin: 0 auto 16px auto;
+  width: 180px;
+  height: 10px;
+}
+.inventory-modal__stub4 {
+  margin: 0 auto 25px auto;
+  width: 80px;
+  height: 10px;
+}
+.delete_button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 220px;
+  height: 40px;
+
+  border-radius: 8px;
+
+  background: #fa7272;
+}
+.delete_button:hover {
+  background: #ff8888;
+}
+.delete_button__text {
+  font-family: "SF Pro Display";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 17px;
+}
+</style>
